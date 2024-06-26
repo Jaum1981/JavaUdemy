c3ee5787bffbe2e03e61f70bd43560f11ba032dc
joão <juaun1981@gmail.com>
2024-06-26 14:04:38 -0300

    adição do método construtor na classe ProductV2

---
+++ Udemy/src/introPOO/application/ProgramProductV2.java
@@ -0,0 +1,23 @@
+package introPOO.application;
+import java.util.Scanner;
+import introPOO.entitie.ProductV2;
+
+public class ProgramProductV2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        int quantity = sc.nextInt();
+        ProductV2 product = new ProductV2(name, price, quantity);
+
+        System.out.println(product);
+
+        sc.close();
+    }
+}
---
+++ Udemy/src/introPOO/entitie/ProductV2.java
@@ -0,0 +1,32 @@
+package introPOO.entitie;
+
+public class ProductV2 {
+    public String name;
+    public double price;
+    public int quantity;
+
+    public ProductV2(String name, double price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public double totalValueInStock(){
+        return price*quantity;
+    }
+
+    public void addProduct(int quantity){
+        this.quantity+=quantity;
+    }
+
+    public void removeProduct(int quantity){
+        this.quantity-=quantity;
+    }
+
+    public String toString(){
+        return name + ", price $ " 
+        + String.format("%.2f", price) + ", quantity " + quantity 
+        + ", Total: $ " + String.format("%.2f", totalValueInStock());
+    }
+
+}
