62f613ee00a01768be2ddd3c593d96bc1d2be8b5
joão <juaun1981@gmail.com>
2024-06-26 16:16:21 -0300

    arrays em java

---
+++ Udemy/src/comportamentoMemoria/ArrayParte1.java
@@ -0,0 +1,27 @@
+package comportamentoMemoria;
+
+import java.util.Scanner;
+
+public class ArrayParte1 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Digite o numero de pessoas para fazer a media da altura: ");
+        int qtd = sc.nextInt();
+        double[] vect = new double[qtd];
+        double sum = 0.0;
+
+        System.out.println("Digite a altura de cada um: ");
+        for(int i = 0; i<qtd; i++){
+            vect[i] = sc.nextDouble();
+            sum += vect[i];
+        }
+
+        double averageHeigt = sum/qtd;
+
+        System.out.println("Average heigt is: " + averageHeigt);
+        sc.close();
+    }
+}
