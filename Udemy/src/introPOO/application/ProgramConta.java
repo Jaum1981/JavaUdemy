cfb3200c412b0ea47cc464b8801b974b5c9507e0
joão <juaun1981@gmail.com>
2024-06-26 16:03:57 -0300

    atividade de fixação

---
+++ Udemy/src/introPOO/application/ProgramConta.java
@@ -0,0 +1,40 @@
+package introPOO.application;
+import java.util.Scanner;
+
+import introPOO.entitie.Conta;
+
+public class ProgramConta {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        Conta conta1;
+
+        System.out.println("Enter number account: ");
+        int numberAccount = sc.nextInt();
+        System.out.println("Enter name account: ");
+        sc.nextLine(); //para consumir a quebra de linha do nextInt()
+        String nameAccount = sc.nextLine();
+        System.out.println("Do you want a initial deposit? (s/n)");
+        char answer = sc.next().charAt(0);
+        if(answer != 'n'){
+            System.out.println("Enter initial deposit: ");
+            double initialDeposit = sc.nextDouble();
+            conta1 = new Conta(numberAccount, nameAccount, initialDeposit);
+        }else{
+            conta1 = new Conta(numberAccount, nameAccount);
+        }
+
+        System.out.println("Enter a deposit value: ");
+        double depositValue = sc.nextDouble();
+        conta1.deposito(depositValue);
+        System.out.println(conta1);
+
+        System.out.println("Enter a withdraw value: ");
+        double withdrawValue = sc.nextDouble();
+        conta1.saque(withdrawValue);
+        System.out.println(conta1);
+
+        sc.close();
+    }
+}
---
+++ Udemy/src/introPOO/entitie/Conta.java
@@ -0,0 +1,48 @@
+package introPOO.entitie;
+
+public class Conta {
+    private int numeroConta;
+    private String nomeTitular;
+    private double saldo;
+
+    public Conta(int numeroConta, String nomeTitular){
+        this.numeroConta = numeroConta;
+        this.nomeTitular = nomeTitular;
+    }
+
+    public Conta(int numeroConta, String nomeTitular, double depositoInicial){
+        this.numeroConta = numeroConta;
+        this.nomeTitular = nomeTitular;
+        deposito(depositoInicial);
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void saque(double valor){
+        this.saldo-=valor + 5.0;
+    }
+
+    public void deposito(double valor){
+        this.saldo += valor;
+    }
+
+    public String toString(){
+        return "Account data: %n" + "Account number:" + getNumeroConta() + ", " 
+        + "Holder: " + getNomeTitular() + ", " + "Balance: $" + String.format("%.2f",getSaldo());
+    }
+    
+}
