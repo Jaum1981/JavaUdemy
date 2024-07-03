c15008e6601e6be253ef30948938f971ae62b3e5
joão <juaun1981@gmail.com>
2024-07-03 08:45:34 -0300

    compreensao de leitura de arquivo em java

---
+++ Udemy/src/arquivos/Leitura.java
@@ -0,0 +1,29 @@
+package arquivos;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String[] args) {
+
+        File file = new File("/Users/jaum/Desktop/JavaUdemy/Udemy/src/arquivos/in.txt");
+        Scanner sc = null;
+        try{
+            sc = new Scanner(file);
+            while (sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch(IOException e) {
+            System.out.println("Error: " + e.getMessage());
+            
+        }
+        finally{
+            if (sc != null) {
+                sc.close();
+            }
+        }
+
+    }
+}
