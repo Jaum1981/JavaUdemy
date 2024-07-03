d2831d4ffe2cc689b60d3c2f9f1e65956e4665db
joão <juaun1981@gmail.com>
2024-07-03 09:38:21 -0300

    ManipulacaoDePastas e InfoArquivo

---
+++ Udemy/src/arquivos/InfoArquivo.java
@@ -0,0 +1,22 @@
+package arquivos;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class InfoArquivo {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a file path: ");
+        String strPath = sc.nextLine();
+
+        File path = new File(strPath);
+
+        System.out.println("Get Name: " + path.getName());//nome 
+        System.out.println("Get Parent" + path.getParent());//caminho
+        System.out.println("Get Path" + path.getPath());//caminho completo
+
+        sc.close();
+    }
+}
---
+++ Udemy/src/arquivos/ManipulacaoDePastas.java
@@ -0,0 +1,33 @@
+package arquivos;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class ManipulacaoDePastas {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a folder path: ");
+        String pathLine = sc.nextLine();
+
+        File path = new File(pathLine);
+
+        File[] folders = path.listFiles(File::isDirectory); //lista so diretorios
+        System.out.println("Folders: ");
+        for (File folder : folders) {
+            System.out.println(folder);
+        }
+
+        File[] files = path.listFiles(File::isFile);
+        System.out.println("Files: ");
+        for (File file : files){
+            System.out.println(file);
+        }
+
+        boolean success = new File(pathLine + "subdir").mkdir(); //cria um subdiretorio(pasta)
+        System.out.println(success);
+
+        sc.close();
+    }
+}
