21f62fa812e8387ffe565f5cb5bd664ad50be6c5
Jaum <juaun1981@alu.ufc.br>
2024-07-16 09:35:32 -0300

    insercao de dados usando jdbc

--- jdbc/dataBase in Java/src/application/Program.java
+++ jdbc/dataBase in Java/src/application/Program.java
@@ -1,7 +1,46 @@
 package application;
 
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import dataBase.DB;
+
 public class Program {
     public static void main(String[] args) {
-        
+
+        PreparedStatement st = null;
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        DB db = new DB();
+        try {
+            db.connectDB();
+            st = db.getConn().prepareStatement(
+                    "INSERT INTO seller "
+                            + "(Name, Email, BirthDate, BaseSalary, Departmentid) "
+                            + "VALUES"
+                            + "(?, ?, ?, ?, ?)" // placeholder
+            );
+            st.setString(1, "Jaum das Neves");
+            st.setString(2, "jaumdasneves@gmail.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("28/09/2002").getTime()));
+            st.setDouble(4, 13463.0);
+            st.setInt(5, 2);
+
+            int rowsAffected = st.executeUpdate();
+            System.err.println("Done! Rows affected: " + rowsAffected);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ParseException pe) {
+            pe.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } finally {
+            db.closeStatement(st);
+            db.closeDatabase();
+        }
     }
 }
--- jdbc/dataBase in Java/src/dataBase/DB.java
+++ jdbc/dataBase in Java/src/dataBase/DB.java
@@ -1,29 +1,58 @@
 package dataBase;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DB {
 
     private Connection conn;
 
-    public void connectDB() {
+    public void connectDB() throws IOException {
 
-        try {
-            String url = "jdbc:mysql://localhost:3306/coursejdbc";
-            String user = "root";
-            String password = "1981";
+        Properties props = new Properties();
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("db.properties")) {
+            if (input == null) {
+                System.err.println("Sorry, unable to find db.properties");
+                return;
+            }
+
+            props.load(input);
+
+            String url = props.getProperty("db.url");
+            String user = props.getProperty("db.user");
+            String password = props.getProperty("db.password");
+
             this.conn = DriverManager.getConnection(url, user, password);
         } catch (SQLException e) {
             e.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
+    }
+
+    public Connection getConn() {
+        return conn;
     }
 
     public void closeDatabase() {
         if (this.conn != null) {
             try {
                 this.conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void closeStatement(PreparedStatement st) {
+        if (st != null) {
+            try {
+                st.close();
             } catch (SQLException e) {
                 e.printStackTrace();
             }
